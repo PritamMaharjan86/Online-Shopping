01ffbf0e69a9a397f6731e9de8bbd6b62dca9e33
Pritam Maharjan <primgdev@gmail.com>
2025-03-23 16:52:17 +1100

    initializing git

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center mt-10">
+                Catering.com
+            </h1>
+            
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
