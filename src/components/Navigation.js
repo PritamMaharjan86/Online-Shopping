26730c3d8bf87a4a27a17af8249e037c684d4093
Pritam Maharjan <primgdev@gmail.com>
2025-03-24 16:38:56 +1100

    Font update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,12 +7,12 @@ const Navigation = () => {
         <div>
             <div class="mx-auto flex max-w-full items-center gap-x-4 bg-white p-6 shadow-lg outline outline-black/5 dark:bg-orange-500 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
                 <div>
-                    <div className="text-5xl font-bold text-black uppercase">Primswed</div>
+                    <div className="text-5xl font-bold text-black uppercase font-Acme">Primswed</div>
                     <p className="text-black text-md uppercase">Your tech store</p>
                 </div>
             </div>
             <div className="mx-auto flex max-w-full items-center gap-x-4 bg-black p-3 shadow-lg outline">
-                <ul className='text-white text-xl flex justify-center space-x-10 m-1'>
+                <ul className='text-white text-lg flex justify-center space-x-10 m-1'>
                     <Link
                         to="/home">
                         Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily:
+    {
+      Acme: ['Acme', 'sans-serif'],
+
+    },
     extend: {},
   },
   plugins: [],
