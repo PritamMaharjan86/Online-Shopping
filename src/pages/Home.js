d5f1d315a48476e76a7ebf011c0d7b1cbd38b39a
Pritam Maharjan <primgdev@gmail.com>
2025-04-05 08:38:49 +1100

    adding design to button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
 
                     <p className='font-bold '>BenQ TK700 4K HDR Gaming Projector</p>
                     <div className='flex flex-col items-center p-8'>
-                        <p className=' border-2 rounded-sm m-8 w-max p-3 pl-10 pr-10 max-h-full items-center bg-yellow-300 font-Russo text-red-600 border-black shadow-lg'>$2395</p>
+                        <p className='border-2 rounded-sm m-8 w-max p-3 pl-10 pr-10 max-h-full items-center bg-yellow-300 font-Russo text-red-600 border-black shadow-[2px_2px_0px_0px_rgb(0,0,0)]'>$2395</p>
 
                         <Button className='bg-black text-white font-bold p-3 max-w-full pl-12 pr-12 ' label="Add to Cart" onClick={() => alert("Product added to cart")}  ></Button>
                     </div>
