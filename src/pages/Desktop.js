ac0057dec6503b4c0c4704588e927a5e0842ae2b
Pritam Maharjan <primgdev@gmail.com>
2025-04-15 19:18:26 +1000

    update

--- src/pages/Desktop.js
+++ src/pages/Desktop.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Desktop = () => {
     return (
         <div className="p-8">
-            <h2 className="text-xl font-bold">Desktops</h2>
-            <p>Showing all desktops here.</p>
+            
         </div>
     )
 }
