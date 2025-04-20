a90500d34dfaa60887b672709878d133b403ae63
Pritam Maharjan <primgdev@gmail.com>
2025-04-20 21:18:59 +1000

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='text-center bg-black text-white py-4'>
-      <span className='font-bold'>Testing the Footer.</span> <br />
-      <span>T&Cs</span><br />
-      <span>All Rights Reserved</span><br />
+     
     </div>
   )
 }
